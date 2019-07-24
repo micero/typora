517759047e92ab9ce6c952e0e31db7fad6d3d31b
Daniel Öbrink <daniel@obrink.me>
2019-07-24 18:54:42 +0200

    Fixed Docker files

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,20 @@
 FROM fedora:latest
 LABEL Description="Fedora RPM Builder"
 
-RUN dnf install -y rpmdevtools git wget
+RUN dnf install -y rpmdevtools git wget expect
 RUN useradd -m -s /bin/bash builder
 
 WORKDIR /home/builder
 
 USER builder
 
-RUN git clone -b master https://github.com/micero/typora.git
+RUN git clone -b master --single-branch https://github.com/micero/typora.git
 
-CMD /bin/bash
+WORKDIR /home/builder/typora
+
+USER root 
+
+CMD ./run-script.sh
     
 
                 
--- run-script.sh
+++ run-script.sh
@@ -1,10 +1,20 @@
-#!/bin/bash
-spawn ./create-package.sh
-expect 'Remove the directory "/home/builder/typora/RPMs"'
+#!/usr/bin/expect
+spawn ./create-package.sh x64
+expect "/home/builder/typora/RPMs"
 send "n\r"
-expect 'Remove the directory "/home/builder/typora/work"?'
+expect "/home/builder/typora/work"
 send "n\r"
-expect 'Use this archive instead of downloading a new one?'
+expect "Use this archive instead of downloading a new one?"
 send "y\r"
-expect 'Install the package now?'
+expect "Install the package now?"
+send "n\r"
+
+spawn ./create-package.sh ia32
+expect "/home/builder/typora/RPMs"
+send "n\r"
+expect "/home/builder/typora/work"
+send "n\r"
+expect "Use this archive instead of downloading a new one?"
+send "y\r"
+expect "Install the package now?"
 send "n\r"
